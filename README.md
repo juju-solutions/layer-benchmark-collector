f861d8295dcf32eaad2e72ab512386e7e666a0a1
Rye Terrell <ryeterrell@ryeterrell.net>
2016-06-03 14:48:27 -0500

    remove collectd

--- README.md
+++ README.md
@@ -4,25 +4,13 @@ This subordinate charm allows for the collection of system and application metri
 
 # Usage
 
-    juju deploy cabs
+    juju deploy benchmark-gui
     juju deploy benchmark-collector
-    juju add-relation cabs benchmark-collector
+    juju add-relation benchmark-gui benchmark-collector
 
     juju deploy mysql
     juju deploy mediawiki
-
     juju add-relation mediawiki:db mysql:db
-    juju add-relation collectd mysql
-    juju add-relation collectd mediawiki
 
-
-# Configuration
-
-    juju set collectd plugins "cpu,memory,disk"
-    juju set collectd extra-config="<Plugin disk>
-        Disk "sda"
-        IgnoreSelected false
-        </Plugin>
-    "
-
-# Contact Information
+    juju add-relation benchmark-collector:beats-host mysql
+    juju add-relation benchmark-collector:beats-host mediawiki
--- reactive/benchmark_collector.py
+++ reactive/benchmark_collector.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 import os
 import sys
 import subprocess
@@ -22,22 +21,19 @@ from charmhelpers.core.templating import render
 hooks = hookenv.Hooks()
 log = hookenv.log
 
-SERVICE = 'collectd'
 COLLECT_PROFILE_DATA = '/usr/local/bin/collect-profile-data'
 
 
 @hook("install")
 def install():
-    log('Installing collectd')
+    log('Installing collector')
     install_packages()
     config_changed()
     write_collect_profile_data_script()
 
+
 @hook("collector-relation-changed")
 def collector_changed(collector):
-    """
-    Connect connectd to the upstream graphite server
-    """
     hostname = hookenv.relation_get('hostname')
     port = hookenv.relation_get('port')
     api_port = hookenv.relation_get('api_port')
@@ -51,43 +47,21 @@ def collector_changed(collector):
     config['collector-web-port'] = api_port
     config.save()
 
-    # We need to get the name of the unit in the collectd relation
-    if 'JUJU_UNIT_NAME' in os.environ:
-        log('Setting up graphite on %s' % os.environ['JUJU_UNIT_NAME'])
-
-        relation_data = hookenv.relations_of_type('benchmark')
-        if relation_data:
-            relation = relation_data[0]["__unit__"]
-            unit_name = "unit-{0}".format(
-                relation.replace('/', '-')
-            )
-
-            if hostname and port:
-                log('Enabling graphite for %s on %s:%s' % (
-                    unit_name, hostname, port))
-                enable_graphite(hostname, port, unit_name)
-
-                # Trigger profile collection
-                write_collect_profile_data_script()
-                collect_profile_data(action_id=action_id)
-
-                start()
-    else:
+    if 'JUJU_UNIT_NAME' not in os.environ:
         log('Unable to get JUJU_UNIT_NAME')
+        return
 
-@hook("collector-relation-departed")
-def collector_departed():
-    if os.path.exists('/etc/collectd/collectd.conf.d/graphite.conf'):
-        os.remove('/etc/collectd/collectd.conf.d/graphite.conf')
-        start()
+    relation_data = hookenv.relations_of_type('benchmark')
+    if relation_data and hostname and port:
+        write_collect_profile_data_script()
+        collect_profile_data(action_id=action_id)
+
 
 @hook("benchmark-relation-changed")
 def benchmark_changed(benchmark):
     config = hookenv.config()
     config['remote-unit'] = os.environ['JUJU_REMOTE_UNIT']
     config.save()
-
-    # Trigger profile collection
     write_collect_profile_data_script()
     collect_profile_data()
 
@@ -146,69 +120,24 @@ def run_command(cmd):
         log('Could not execute command: %s' % cmd)
     return output
 
+
 @hook("config-changed")
 def config_changed():
     config = hookenv.config()
-
     for key in config:
         if config.changed(key):
             log("config['{}'] changed from {} to {}".format(
                 key, config.previous(key), config[key]))
-
-    # Write active plugins
-    if config.changed('plugins'):
-        plugins = []
-        for plugin in config['plugins'].split(','):
-            if len(plugin.strip()):
-                plugins.append(plugin)
-        render(
-            source="plugins.tmpl",
-            target="/etc/collectd/collectd.conf.d/plugins.conf",
-            context={
-                "plugins": plugins
-            }
-        )
-    if config.changed('extra-config'):
-        host.write_file(
-            '/etc/collectd/collectd.conf.d/extra.conf',
-            bytes("%s\n" % config['extra-config'], "utf-8")
-        )
-
     config.save()
-    start()
+
 
 @hook("upgrade-charm")
 def upgrade_charm():
-    log('Upgrading collectd')
+    log('Upgrading benchmark-collector')
     install_packages()
     config_changed()
-
-@hook("start")
-def start():
-    host.service_restart(SERVICE) or host.service_start(SERVICE)
-
-@hook("stop")
-def stop():
-    host.service_stop(SERVICE)
 
 
 def install_packages():
     apt_update(fatal=True)
-    apt_install(packages=['collectd', 'python-setuptools'], fatal=True)
-
-
-def enable_graphite(hostname, port, unit_name):
-    # Enable/configure whisper plugin
-    target = "/etc/collectd/collectd.conf.d/graphite.conf"
-    render(
-        source="graphite.tmpl",
-        target=target,
-        context={
-            "host": hostname,
-            "port": port,
-            "unit": unit_name
-        }
-    )
-    # write a newline because collectd wants it
-    with open(target, "a") as f:
-        f.write("\n")
+    apt_install(packages=['python-setuptools'], fatal=True)
